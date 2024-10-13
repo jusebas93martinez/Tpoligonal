fba82659dcb4fe7ddafe84a5eb476208d8dd7725
Sebastián Martinez <sebas93martinez@gmail.com>
2024-10-12 19:34:20 -0500

    Updates

---
+++ precache-manifest.6cf1c7c4ae4ade1fde906c60474e41bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddc1858ebd39da55135f14663dbd036",
+    "url": "/Tpoligonal/index.html"
+  },
+  {
+    "revision": "fd5151d12faacd6277dc",
+    "url": "/Tpoligonal/static/css/main.87013bea.chunk.css"
+  },
+  {
+    "revision": "9e64758407d967c5bda1",
+    "url": "/Tpoligonal/static/js/2.0d071e9f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Tpoligonal/static/js/2.0d071e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afac8373187386cf6cde",
+    "url": "/Tpoligonal/static/js/3.0406087f.chunk.js"
+  },
+  {
+    "revision": "fd5151d12faacd6277dc",
+    "url": "/Tpoligonal/static/js/main.f0b2ccb6.chunk.js"
+  },
+  {
+    "revision": "ef02cddcf7a234d6af60",
+    "url": "/Tpoligonal/static/js/runtime-main.68246cd1.js"
+  }
+]);
